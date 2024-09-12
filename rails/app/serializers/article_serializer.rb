3123d85d1b0bb86860b17ce6710940edb6725536
nargok <nargok@gmail.com>
2024-09-12 17:10:08 +0900

    article show action

--- rails/app/serializers/article_serializer.rb
+++ rails/app/serializers/article_serializer.rb
@@ -1,34 +1,45 @@
 class ArticleSerializer
   include Alba::Resource
 
-  attributes :id, :title, :content, :created_at, :from_today
+  attributes :id, :title, :content
 
-  def created_at
-    object.created_at.strftime("%Y/%m/%d")
+  attribute :created_at do |resource|
+    resource.created_at.strftime("%Y/%m/%d")
   end
 
-  def from_today
+  attribute :from_today do |resource|
     now = Time.zone.now
-    created_at = object.created_at
+    created_at = resource.created_at
 
     months = (now.year - created_at.year) * 12 + now.month - created_at.month - ((now.day >= created_at.day) ? 0 : 1)
     years = months.div(12)
 
-    return "#{years}年前" if years > 0
-    return "#{months}ヵ月前" if months > 0
-
-    seconds = (Time.zone.now - object.created_at).round
-
-    days = seconds / (60 * 60 * 24)
-    return "#{days}日前" if days.positive?
-
-    hours = seconds / (60 * 60)
-    return "#{hours}時間前" if hours.positive?
-
-    minutes = seconds / 60
-    return "#{minutes}分前" if minutes.positive?
-
-    "#{seconds}秒前"
+    if years > 0
+      "#{years}年前"
+    elsif months > 0
+      "#{months}ヵ月前"
+    else
+      seconds = (Time.zone.now - resource.created_at).round
+      days = seconds / (60 * 60 * 24)
+      if days.positive?
+        "#{days}日前"
+      else
+        hours = seconds / (60 * 60)
+        if hours.positive?
+          "#{hours}時間前"
+        else
+          minutes = seconds / 60
+          if minutes.positive?
+            "#{minutes}分前"
+          else
+            "#{seconds}秒前"
+          end
+        end
+      end
+    end
   end
 
+  attribute :user do |resource|
+    UserSerializer.new(resource.user).as_json
+  end
 end
