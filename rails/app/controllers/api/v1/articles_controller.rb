21c65bf49f3f2c661eadd2718487e6d11743a2ee
nargok <nargok@gmail.com>
2024-09-12 17:20:49 +0900

    article index action

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::ArticlesController < ApplicationController
+  def index
+    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10).eager_load(:user)
+    render json: articles.map { |article| ArticleSerializer.new(article).as_json }
+  end
+
   def show
     article = Article.published.find(params[:id])
     render json: ArticleSerializer.new(article).serialize
