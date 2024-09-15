0df07e14df165db549d34821f015dc4ed6ac08bc
nargok <nargok@gmail.com>
2024-09-16 08:56:42 +0900

    Update my article

--- rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -5,4 +5,16 @@ class Api::V1::Current::ArticlesController < Api::V1::BaseController
     unsaved_article = UnsavedArticle.new(current_user).get || UnsavedArticle.new(current_user).register!
     render json: ArticleSerializer.new(unsaved_article).as_json
   end
+
+  def update
+    article = current_user.articles.find(params[:id])
+    article.update!(article_params)
+    render json: article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :status)
+  end
 end
