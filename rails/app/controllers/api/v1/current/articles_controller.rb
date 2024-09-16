e5bf47948a6e28aea506e09468fd3ab83d08d518
nargok <nargok@gmail.com>
2024-09-16 09:07:32 +0900

    my article index & show

--- rails/app/controllers/api/v1/current/articles_controller.rb
+++ rails/app/controllers/api/v1/current/articles_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::Current::ArticlesController < Api::V1::BaseController
   before_action :authenticate_user!
 
+  def index
+    articles = current_user.articles.not_unsaved.order(created_at: :desc)
+    render json: ArticleSerializer.new(articles).as_json
+  end
+
+  def show
+    article = current_user.articles.find(params[:id])
+    render json: ArticleSerializer.new(article)
+  end
+
   def create
     unsaved_article = UnsavedArticle.new(current_user).get || UnsavedArticle.new(current_user).register!
     render json: ArticleSerializer.new(unsaved_article).as_json
