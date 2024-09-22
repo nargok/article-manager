8bb727f55ea7ed0f9a1ef69ac1a3e6d07a92535e
nargok <nargok@gmail.com>
2024-09-22 12:43:17 +0900

    typo

--- next/src/pages/api/health_check.ts
+++ next/src/pages/api/health_check.ts
@@ -2,6 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'applicaton/json')
+  res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({ status: 'ok' }))
 }
